8ae22e6276debca09c4764302bcff32a92974bf8
Guilherme Siquinelli (guiseek) <guiseek@gmail.com>
2021-10-22 13:51:25 -0300

    fix: base href

--- custom/head-common.js
+++ custom/head-common.js
@@ -1,16 +1,22 @@
 customElements.define('head-common',
   class extends HTMLElement {
     scripts = [
-      { src: '/third-party/prism/prism.min.js' }
+      { src: 'third-party/prism/prism.min.js' }
     ]
     links = [
       { rel: 'preconnect', href: 'https://fonts.googleapis.com ' },
       { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap' },
-      { rel: 'stylesheet', href: '/third-party/prism/prism.css' }
+      { rel: 'stylesheet', href: 'third-party/prism/prism.css' }
       
     ];
     connectedCallback() {
+      console.log(document.head.baseURI);
+      if (document.head.baseURI.indexOf('github') > -1) {
+        const base = document.createElement('base');
+        base.setAttribute('href', '/bootcamp-2021/');
+        document.head.appendChild(base);
+      }
 
       this.links.map(({ rel, href, crossorigin}) => {
         const link = document.createElement('link');
