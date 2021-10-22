1de8859d41407eff8aea1483d46c89453deed124
guiseek <guiseek@users.noreply.github.com>
2021-10-22 16:38:07 +0000

    deploy: 580cbfedea1414fb49f0a3f069395c65274f26f1

---
+++ custom/head-common.js
@@ -0,0 +1,39 @@
+customElements.define('head-common',
+  class extends HTMLElement {
+    scripts = [
+      { src: '/third-party/prism/prism.min.js' }
+    ]
+    links = [
+      { rel: 'preconnect', href: 'https://fonts.googleapis.com ' },
+      { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap' },
+      { rel: 'stylesheet', href: '/third-party/prism/prism.css' }
+      
+    ];
+    connectedCallback() {
+
+      this.links.map(({ rel, href, crossorigin}) => {
+        const link = document.createElement('link');
+
+        link.setAttribute('rel', rel);
+        link.setAttribute('href', href);
+
+        if (typeof crossorigin === 'string') {
+          link.setAttribute('crossorigin', crossorigin);
+        }
+        
+        document.head.appendChild(link);
+      })
+      
+      this.scripts.map(({ src, type }) => {
+        const script = document.createElement('script');
+
+        script.setAttribute('src', src);
+
+        if (type) script.setAttribute('type', type);
+        
+        document.body.appendChild(script);
+      })
+    }
+  }
+)
